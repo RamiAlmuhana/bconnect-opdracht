d740061ee05d9a296328195eaf12cf1f5e220c6e
RamiAlmuhana <ramealmuhana@hotmail.com>
2025-05-28 15:13:43 +0200

    project init

---
+++ bconnect-opdracht-backend/src/main/java/com/rami/bconnectopdrachtbackend/BconnectOpdrachtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rami.bconnectopdrachtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BconnectOpdrachtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BconnectOpdrachtBackendApplication.class, args);
+    }
+
+}
---
+++ bconnect-opdracht-backend/src/test/java/com/rami/bconnectopdrachtbackend/BconnectOpdrachtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rami.bconnectopdrachtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BconnectOpdrachtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
